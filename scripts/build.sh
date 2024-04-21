808a974b058911c0fab31cc5fe6c1a8594a5820d
Jarell <91372088+jarelllama@users.noreply.github.com>
2024-04-21 22:07:11 +0800

    Build.sh: update BSD license year

--- scripts/build.sh
+++ scripts/build.sh
@@ -104,7 +104,7 @@ append_header() {
 !
 ! LICENSE:
 ! Emerging Threats community rules, from which this hosts file is derived, are BSD-licensed:
-!  Copyright (c) 2003-2021, Emerging Threats
+!  Copyright (c) 2003-2024, Emerging Threats
 !  All rights reserved.
 !
 !  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
